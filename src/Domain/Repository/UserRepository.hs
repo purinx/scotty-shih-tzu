d821a481345dc0041bc914aefee367f079e43e87
kijimaru <eddix.ibanez@outlook.jp>
2020-04-05 01:51:08 +0900

    impl password insert

--- src/Domain/Repository/UserRepository.hs
+++ src/Domain/Repository/UserRepository.hs
@@ -4,24 +4,31 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Domain.Repository.UserRepository where
 
+import Crypto.KDF.BCrypt (hashPassword, validatePassword)
 import Data.Aeson (FromJSON)
 import Data.Text (Text)
+import Data.Text as T
+import Data.Text.Encoding
+import Data.Aeson
 import Database.MySQL.Base
 import Database.MySQL.Protocol.MySQLValue
 import Domain.Entity.User
 import GHC.Generics
 import GHC.Word
 import Text.Read (readMaybe)
+import qualified Data.ByteString as B
+import Data.ByteString.Char8 as C8
 import qualified System.IO.Streams as Streams
 
 data CreateUserDto = CreateUserDto {
-  cuName :: Text
+  cuName :: Text,
+  cuPassword :: Text
 } deriving (Show, Generic)
 
 instance FromJSON CreateUserDto
 
 createEntity :: [MySQLValue] -> Maybe User
-createEntity (MySQLInt32 uid : MySQLText name : _) = Just User { 
+createEntity (MySQLInt32 uid : MySQLText name : _) = Just User {
   uid = readMaybe $ show uid
 , name
 }
@@ -31,9 +38,10 @@ findById :: Int -> MySQLConn -> IO [Maybe User]
 findById uid conn = do
   s <- prepareStmt conn "SELECT * FROM users WHERE id = ?"
   (defs, is) <- queryStmt conn s [MySQLInt32U $ fromIntegral uid]
-  map createEntity <$> Streams.toList is
+  Prelude.map createEntity <$> Streams.toList is
 
 createUser :: CreateUserDto -> MySQLConn -> IO(OK)
 createUser user conn = do
-  s <- prepareStmt conn "INSERT INTO users (name) values (?)"
-  executeStmt conn s [MySQLText $ cuName user]
+  s <- prepareStmt conn "INSERT INTO users (name, password) values (?, ?)"
+  passwordHash <- hashPassword 12 $ C8.pack $ T.unpack $ cuPassword user
+  executeStmt conn s [MySQLText $ cuName user, MySQLBytes passwordHash]
